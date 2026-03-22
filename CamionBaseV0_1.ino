86d82ba4cf3a4a3f7c028bdb427cf6b9793b4ce4
AitorLazarraga <alp1023@alu.ubu.es>
2026-03-22 14:04:33 +0100

    Integrate Modulino library and enhance sensor readings
    Updated the code to include the Modulino library and improved sensor reading functionality. Added support for reading distance from a ToF sensor.

--- CamionBaseV0_1.ino
+++ CamionBaseV0_1.ino
@@ -2,6 +2,8 @@
 #include <Arduino_RouterBridge.h>
 #include <Wire.h>
 #include "truck.h"
+#include <Modulino.h>
+
 
 
 // Defines
@@ -14,6 +16,7 @@ const uint8_t ECHO_PINS[] = {6, 7, 8, 9, 10, 11, 12, 13};
 const size_t NUM_SENSORS = sizeof(ECHO_PINS) / sizeof(ECHO_PINS[0]);
 // Classes
 //VL53L4CD_Custom tof;
+ModulinoDistance distance; // Se vuelve a usar la libreria modulino tras actualización que arregla el fallo de compatibildiad
 
 uint8_t status;
 uint16_t data[7];
@@ -21,6 +24,7 @@ String msg = "";
 
 
 void controlMotores(String cmd){
+  // Envio de datos por serial al Arduino Mega
   Serial.println(cmd);
 }
 
@@ -40,11 +44,20 @@ void setup() {
   delay(1000);
   Serial.println("UNO Listo");
   Bridge.provide("controlMotores", controlMotores);
+  Modulino.begin();
+  distance.begin();
 }
 
 
 void loop() {
-  
+  //Arduino lee continuamente para obtener los datos del sensor IMU desde el Arduino Mega
+  if(Serial.available()){
+    String cmd = Serial.readStringUntil('\n');
+    cmd.trim();
+    Bridge.notify("parse", cmd);
+  }
+
+  //Se lee en cada iteración cada sensor ultrasonico
   for (size_t i = 0; i < NUM_SENSORS; i++) {
     long pulse = pulse_in(TRIGGER_PIN, ECHO_PINS[i]);
     if (pulse > 0) {
@@ -53,6 +66,13 @@ void loop() {
       Bridge.notify("distance", envio);
     }
     delay(60);
+  }
+  //Se lee en cada iteración el sensor ToF trasero
+  if (distance.available()) {
+    int measure = distance.get();
+    measure = measure / 10;
+    String envio = "M:" + String(measure);
+    Bridge.notify("distance", envio);
   }
   delay(500);
 }
